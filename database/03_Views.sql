7655e51b48c01b242a03028ac9b5b55884c1cb4e
nnhanfptdn-thanhco <nnhanfptdn@gmail.com>
2026-06-22 21:30:33 +0700

    feat(database): create view for staff and assigned shifts

---
+++ database/03_Views.sql
@@ -0,0 +1,9 @@
+﻿USE StaffShiftManagement;
+GO
+
+CREATE VIEW vw_Staff_Shifts AS
+SELECT s.FullName, sh.ShiftName, sh.Time_Range
+FROM STAFF s
+JOIN SCHEDULES sc ON s.UserID = sc.UserID
+JOIN SHIFT sh ON sc.ShiftID = sh.ShiftID;
+GO
